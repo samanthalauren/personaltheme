a0d8caf0bcedcec987b2bf6b447175b62d10485d
Samantha Cross <samm.cross@gmail.com>
2015-11-05 21:53:12 -0500

    more styles

--- page-portfolio.php
+++ page-portfolio.php
@@ -5,12 +5,16 @@
 
     <div class="content">
       <?php // Start the loop ?>
-     <div class="first">
+     <div class="portfolioFirst">
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
         <?php the_content(); ?>
         <div class="portfolio-gallery" >
         <?php while( has_sub_field('portfolio')): ?>
           <div class="gallery-cell">
+          <div class="description"></div>
+            <div class="description"></div>
+            <div class="description"></div>
+            <div class="description"></div>
             <img src="<?php the_sub_field('pitem'); ?>" alt="">
             <p><?php the_sub_field('ptools'); ?></p>
             <p><?php the_sub_field('pblurb'); ?></p>
